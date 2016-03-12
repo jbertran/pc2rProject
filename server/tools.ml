bca8eb3aa9092df0a69a3ea307b0429cb845f9d0
Jordi <j.debalanda@gmail.com>
2016-03-12 12:10:14 +0100

    updated tools for new file format

--- server/tools.ml
+++ server/tools.ml
@@ -1,8 +1,25 @@
+open Str;;
+
 let read_walls_file filepath = 
-  let reader = open_in filepath in
-  let wall_list = ref [] in
-  try
-    while true; do
+  let reader = open_in filepath 
+  and line = ref "" 
+  and wall_list = ref [] in
+  try 
+    line := input_line reader;
+    let h::w::[] = split (regexp "[ \t]+") !line in
+    while (not (string_match (regexp "-1 -1") !line 0)); do
+      let sp1::sp2::sp3::[] = split (regexp "[ \t]+") !line in
+      let d = match sp3 with "H" -> GameState.H
+                           | "B" -> GameState.B
+                           | "G" -> GameState.G
+                           | "D" -> GameState.D
+                           | _ -> GameState.D
+      in
+      wall_list := ((int_of_string sp1), (int_of_string sp2), d)::(!wall_list);
+      line := input_line reader
+    done;
+    (*
+      while true; do
       let line = input_line reader in
       let line_noparen = String.sub line 1 ((String.length line) - 2) in
       let split = Str.split (Str.regexp ",") line_noparen in
@@ -15,6 +32,9 @@ let read_walls_file filepath =
       in
       wall_list := ((int_of_string sp1), (int_of_string sp2), d)::(!wall_list);
     done;
+       *)
+    !wall_list;
+    close_in reader;
     !wall_list
   with End_of_file -> 
     close_in reader;
